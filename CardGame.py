50c1f84debbb1fe3d527f2bf86c859b7030973ec
HienaDev <ant.rodrigues1999@gmail.com>
2022-11-03 14:49:23 +0000

    CardGame First Iteration

---
+++ CardGame.py
@@ -0,0 +1,30 @@
+import random
+
+DeckPlayer = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+DeckNPC = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+def showDeck(Deck):
+    
+    print(str(Deck))
+
+def shuffleDeck (Deck):
+
+    random.shuffle(Deck)
+
+def goFish(Deck):
+    
+    print("He gets " + str(Deck[12]) + " and " + str(Deck[13]))
+
+
+
+shuffleDeck(DeckPlayer)
+print("Player's Shuffled Deck: ")
+showDeck(DeckPlayer)
+print("\n")
+shuffleDeck(DeckNPC)
+print("NPC's Shuffled Deck: ")
+showDeck(DeckNPC)
+print("\nPlayer goes fish: ")
+goFish(DeckPlayer)
+print("\nNPC goes fish: ")
+goFish(DeckNPC)
