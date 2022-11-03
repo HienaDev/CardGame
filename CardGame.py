cea9ee9e71f1ffcadfadd1815005a2ce328e86d0
HienaDev <ant.rodrigues1999@gmail.com>
2022-11-03 16:40:48 +0000

    Card Game added discard option

--- CardGame.py
+++ CardGame.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 DeckPlayer = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 DeckNPC = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -14,8 +15,7 @@ def shuffleDeck (Deck):
 
 def goFish(Deck):
     
-    print("He gets " + str(Deck[12]) + " and " + str(Deck[13]))
-    return (Deck[12], Deck[13])
+    return (Deck[len(Deck) - 2], Deck[len(Deck) - 1])
 
 def isNumber(phrase):
 
@@ -32,19 +32,52 @@ def lastCardSum(cards):
     sumFinal = sumFinal + isNumber(cards[1])
     
     for i in range(len(Test)):
-        if((cards[0], cards[1])  == Test[i]):
+        if(Test[i] in (cards[0], cards[1])):
             sumFinal = sumFinal + 11 + i
 
     return(sumFinal)
 
 def whoWins(player, npc):
+
+    print("Player sum: " + str(player) + " \nNPC sum: " + str(npc))
+    print("------------------------------------------")
     if (player > npc):
+        print("Player Wins!")
         return (1)
     elif (player < npc):
+        print("NPC Wins!")
         return (-1)
     else:
+        print("It's a tie!")
         return(0)
 
+
+def discardCard(hand):
+
+    while (True):
+        discard = input("How many cards would you like to discard? Your hand is " + hand[0] + " and " + hand[1] + "\n").translate({ord(c): None for c in string.whitespace})
+        if (discard == "0"):
+            break
+        elif (discard == "1"):
+            whatDiscard = input("\nWhat card do you want to discard? 1) " + hand[0] + " 2) " + hand[1] + "\n").translate({ord(c): None for c in string.whitespace})
+            if (whatDiscard == "1"):
+                hand = (goFish(DeckPlayer[:12])[1], hand[1])
+                print("You get " + goFish(DeckPlayer[:12])[1] + "!")
+            elif (whatDiscard == "2"):
+                hand = (hand[0], goFish(DeckPlayer[:12])[1])
+                print("You get " + goFish(DeckPlayer[:12])[1] + "!")
+            else:
+                print("Invalid Choice")
+                continue
+            break
+        elif (discard == "2"):
+            hand = goFish(DeckPlayer[:12])
+            print("You get " + str(hand))
+            break
+        else:
+            print("Invalid Choice")
+    
+    return (hand)
 
 shuffleDeck(DeckPlayer)
 print("Player's Shuffled Deck: ")
@@ -54,8 +87,10 @@ shuffleDeck(DeckNPC)
 print("NPC's Shuffled Deck: ")
 showDeck(DeckNPC)
 print("\nPlayer goes fishing: ")
-playerSum = goFish(DeckPlayer)
+playerHand = goFish(DeckPlayer)
+print(str(playerHand))
 print("\nNPC goes fishing: ")
-NPCSum = goFish(DeckNPC)
-#print("---------------\nWho wins: " + str(lastCardSum(DeckPlayer, DeckNPC)))
-print("---------------\nWho wins: " + str(whoWins(playerSum, NPCSum)))
+NPCHand = goFish(DeckNPC)
+print(str(NPCHand))
+playerHand = discardCard(playerHand)
+whoWins(lastCardSum(playerHand), lastCardSum(NPCHand))
